53ac1227488619b6c96ed35296a0bc1255992eeb
Onyela <onyela.ogah@madetech.com>
2021-09-08 16:34:17 +0100

    finish palindrome detector

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 let Phrase = require("mhartl-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:")
-let phrase = new Phrase(string);
 
-if (phrase.palindrome()){
-    alert(`${phrase.content} is a palindrome. Yay!`)
-} else {
-    alert(`Sorry, ${phrase.content} isn't a palindrome.`)
+function palindromeTester(event){
+    event.preventDefault();
+
+    let phrase = new Phrase(event.target.phrase.value);
+    let palindromeResult = document.querySelector("#palindromeResult");
+
+    if (phrase.palindrome()) {
+        palindromeResult.innerHTML = `${phrase.content} is a palindrome. Yay!`;
+    } else {
+        palindromeResult.innerHTML =`Sorry, ${phrase.content} isn't a palindrome.`;
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    let button = document.querySelector("#palindromeTester");
+
+    button.addEventListener("submit", function () {
+        palindromeTester(event);
+    });
+});
+
 
 
